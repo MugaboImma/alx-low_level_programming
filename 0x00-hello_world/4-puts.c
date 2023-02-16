6ffaa5b3c192c2ff349f34118fc4a01fb5760b4a
MugaboImma <emugabo21@yahoo.com>
2023-02-16 09:29:29 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <sdtio.h>
+/**
+ * main - The program that will print a line with puts function
+ * Return: 0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
