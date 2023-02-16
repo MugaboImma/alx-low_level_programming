52a98414bce2aaa48b48ed0515359a71cab48e02
MugaboImma <emugabo21@yahoo.com>
2023-02-16 12:40:03 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,6 +3,7 @@
  * main - This prints a line using printf function
  *  Return: 0 (successful printed)
  */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art, \n");
