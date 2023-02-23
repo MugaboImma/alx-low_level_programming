6bbe96ab0b9692db8ac25f543639dbab0382dc09
MugaboImma <emugabo21@yahoo.com>
2023-02-23 06:16:43 -0800

    Multiple mains

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - A file to test for a positive_or_negative function
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,2 @@
+** 0x03. C - Debugging **
+This project covers what is debugging, the medoths of debugging manually, and the error messages
